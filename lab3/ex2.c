483841816359584df918db1ad71c652781ecf0b6
Luan Gibson <luan.gibson.coutinho@gmail.com>
2025-09-18 15:35:32 +0000

    Adicionando lab 7

--- lab3/ex2.c
+++ lab3/ex2.c
@@ -1,19 +1,29 @@
+#include <stdio.h>
+
 int odd_ones(unsigned int x){
 
     int count;
-for (int i = 0; i < 31; i++)
-{
-count= count+ (x&1);
-x=x>>1;
+    for (int i = 0; i < 32; i++)
+    {
+        count= count+ (x&1);
+        x=x>>1;
+    }
+
+    if (count&1){
+        printf("Impar");
+        return 1;
+    }
+    else{
+        printf("Par");
+        return 0;
+
+    }
+
+
 }
 
-if (count&1){
-printf("Impar");
-
-}
-else{
-printf("Par");
-
-}
-
+int main(void) {
+  printf("%x tem numero %s de bits\n",0x01010101,odd_ones(0x01010101) ? "impar":"par");
+  printf("%x tem numero %s de bits\n",0x01030101,odd_ones(0x01030101) ? "impar":"par");
+  return 0;
 }

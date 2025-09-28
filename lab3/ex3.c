2fdd0bfb7b9d00b35196534ca2829af637b93d88
Luan Gibson <luan.gibson.coutinho@gmail.com>
2025-09-28 22:32:58 +0000

    Ex3 b terminado + relatorio da mesma

--- lab3/ex3.c
+++ lab3/ex3.c
@@ -16,6 +16,10 @@ return res;
 
 }
 
+unsigned char rotate_left(unsigned char x, int n) {
+    return (x << n) | (x >> (8 - n));
+}
+
 int main(void) {
     unsigned char v1 = 0xAB;
     unsigned char v2 = 0xF0;
@@ -24,6 +28,13 @@ int main(void) {
     printf("0x%X -> 0x%X\n", v1, switch_byte(v1));
     printf("0x%X -> 0x%X\n", v2, switch_byte(v2));
     printf("0x%X -> 0x%X\n", v3, switch_byte(v3));
+
+    unsigned char v = 0x61; // 0110 0001
+
+    printf("0x61 rotate left 1 bit: 0x%X\n", rotate_left(v, 1)); // 0xC2
+    printf("0x61 rotate left 2 bits: 0x%X\n", rotate_left(v, 2)); // 0x85
+    printf("0x61 rotate left 7 bits: 0x%X\n", rotate_left(v, 7)); // 0xB0
+
 
     return 0;
 }
